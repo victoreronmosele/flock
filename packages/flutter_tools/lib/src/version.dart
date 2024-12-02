83229b7aaf3ea85edd8983feb00957e1884099f1
Victor Eronmosele <victoreronmosele@gmail.com>
2024-12-02 03:08:28 +0100

    testing: to test upgrade

--- packages/flutter_tools/lib/src/version.dart
+++ packages/flutter_tools/lib/src/version.dart
@@ -714,6 +714,7 @@ class VersionUpstreamValidator {
     'https://github.com/victoreronmosele/flock.git',
     'git@github.com:victoreronmosele/flock.git',
     'ssh://git@github.com/victoreronmosele/flock.git',
+    
   ];
 
   // Strips ".git" suffix from a given string, preferably an url.
